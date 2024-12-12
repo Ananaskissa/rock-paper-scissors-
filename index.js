423846287e0ea724df9bdc9c66f3cd4b7ba7f203
vadelmakissa <vadelmakissa@gmail.com>
2024-12-12 21:05:21 +0200

    lisätty peliin funktioita. yhden erän voi jo pelata

--- index.js
+++ index.js
@@ -3,7 +3,40 @@ function getComputerChoice() {
     let value = options[Math.floor(Math.random() * options.length)];
 
     return value
-            
+    
 }
 
-console.log(getComputerChoice())
+
+function getHumanChoice(){
+    let Choice = prompt("Choose rock, paper or scissors");
+        return Choice
+}
+
+
+humanScore = 0
+computerScore = 0
+
+
+
+function playRound(humanChoice, computerChoice){
+
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === computerChoice)
+        { console.log("Draw"); }
+    else if (humanChoice === "rock" && computerChoice === "paper")
+       { console.log(`You lose! ${computerChoice} wins ${humanChoice}`);
+        computerScore ++; }
+    else if (humanChoice === "scissors" && computerChoice === "rock")
+       { console.log(`You lose! ${computerChoice} wins ${humanChoice}`);
+        computerScore ++; } 
+    else if (humanChoice === "paper" && computerChoice === "scissors")
+    {   console.log(`You lose! ${computerChoice} wins ${humanChoice}`);
+        computerScore ++; }
+    else { console.log(`You won! ${humanChoice} wins ${computerChoice}`);
+
+    }
+
+}
+
+playRound(getHumanChoice(), getComputerChoice())
